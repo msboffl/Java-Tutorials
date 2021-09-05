b71bf93350d8e300c124f7bd8c5f3f56ebaf7e32
Mareedu Saibabu <marsaiba@publicisgroupe.net>
2021-09-05 12:29:49 +0530

    Java Tutorials

---
+++ src/forSample.java
@@ -0,0 +1,8 @@
+public class forSample {
+    public static void main(String [] args) {
+        int x,y;
+        for (x = 0; x<10; x++) {
+            System.out.println("This x value is :" + x );
+        }
+    }
+}
---
+++ src/multiplideByTwo.java
@@ -0,0 +1,10 @@
+public class multiplideByTwo {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("This is num :" +num);
+        num = num*2;
+        System.out.println("Ths value of num * 2 is");
+        System.out.println(num);
+    }
+}
